13efc85fdffbd75278b76132e536485ee781b943
pme1123 <pmewing87@gmail.com>
2016-12-27 12:42:54 -0600

    License, setup script, and sample images

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name = 'pyroots',
+      version = '0.1',
+      description = """Tools for measuring the length of objects
+      like roots or hyphae in images""",
+      url = 'https://github.com/pme1123/PyRoots',
+      author = "Patrick M. Ewing",
+      author_email = 'pme1123@yahoo.com',
+      license = 'Adobe 2.0',
+      packages = ['pyroots'],
+      zip_safe = False)
