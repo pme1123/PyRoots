7f533096ab875cc28296a4b6aa4c4f06f2857d4e
pme1123 <pmewing87@gmail.com>
2016-12-27 22:59:22 -0600

    update __init__.py to include example_functions.py

--- pyroots/__init__.py
+++ pyroots/__init__.py
@@ -4,6 +4,7 @@ from .summarize import summarize_geometry, bin_by_diameter
 from .skeletonizatoin import _axis_length, skeletonize_with_distance
 from .image_manipulation import img_split, circle_mask
 from .utilities import multi_image_plot, random_blobs
+from .example_functions import pyroots_analysis, multi_image_loop
 
 from .image_loop import image_loop
 from .whole_shebang import whole_shebang
@@ -14,4 +15,4 @@ __all__ = ['noise_removal', 'dirt_removal',
 	   	   '_axis_length', 'skeletonize_with_distance',
 	   	   'img_split', 'circle_mask',
 	   	   'multi_image_plot', 'random_blobs',
-	   	   'image_loop', 'whole_shebang']
+	   	   'pyroots_analysis', 'multi_image_loop']
