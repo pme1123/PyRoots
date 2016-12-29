88b7770fb38e9dc1b90019ecfedb879e9d796c35
pme1123 <pmewing87@gmail.com>
2016-12-29 13:20:08 -0600

    rework filters, skeletonization to use and update common dictionaries

--- pyroots/geometry_filters.py
+++ pyroots/geometry_filters.py
@@ -70,7 +70,7 @@ def _percentile_filter(labels, diameter_image, percentile, value, test_type):
     return(out)
 
 
-def diameter_filter(diameter_image, length_image, objects_image,
+def diameter_filter(skeleton_dictionary,
             max_diameter=1000, min_diameter=-1, 
             max_percentile=100, min_percentile=None, pixel_level = False):
     """
@@ -82,15 +82,14 @@ def diameter_filter(diameter_image, length_image, objects_image,
     
     Parameters
     ----------
-    diameter_image : array
-        the diameter skeleton array of the thresholded (object) image, for example
-        ``pyroots.skeleton_with_distance(binary_image)[1]``
-    length_image : array
-        the length skeleton array of the thresholded (object)image, for example
-        ``pyroots.skeleton_with_distance(binary_image)[0]``
-    objects_image : array
-        Binary array of objects, for example feeding into 
-        ``pyroots.skeleton_with_distance``.
+    skeleton_dictionary : dict
+        Standard dictionary of objects returned from ``pyroots.skeleton_with_distance``.
+        Items are: 
+            * "objects", a binary image of objects
+            * "length", an ndarray of medial axis, with each pixel representing length
+            * "diameter", an ndarray of medial axis, with each pixel representing diameter
+            * "geometry", a pandas ``DataFrame`` of total length and average diameter for
+            each object.
     max_percentile : float
         Of all of the skeleton pixels for a single object, if the ceiling is smaller 
         than the percentile, the entire object is deleted. Feeds into 
@@ -123,22 +122,25 @@ def diameter_filter(diameter_image, length_image, objects_image,
     ``pyroots._percentile_filter``, ``scipy.ndimage.label``, ``pandas``
     
     """    
+    diameter_in = skeleton_dictionary["diameter"]
+    length_in = skeleton_dictionary["length"]
+    objects_in = skeleton_dictionary["objects"]
 
-    # make sure diameter, length skeletons match objects image
-    diameter_image = diameter_image * objects_image
-    length_image = length_image * objects_image
+    # make sure skeletons for diameter and length are updated with objects
+    diameter = diameter_in * objects_in
+    length = length_in * objects_in
     
     #Label objects for indexing
-    labels, labels_ls= ndimage.label(diameter_image > 0,
+    labels, labels_ls= ndimage.label(diameter > 0,  # convert float to boolean
                                      structure = np.ones((3,3))) #square for skeletons
       
     ####  Percentile filters  ####              
-    max_perc_filter = _percentile_filter(labels, diameter_image, 
+    max_perc_filter = _percentile_filter(labels, diameter, 
                                          max_percentile, max_diameter,
                                          'ceiling')
                     
     if min_percentile is not None:
-        min_perc_filter = _percentile_filter(labels, diameter_image,
+        min_perc_filter = _percentile_filter(labels, diameter,
                                              min_percentile, min_diameter, 
                                              'floor')
         perc_filter = max_perc_filter * min_perc_filter
@@ -146,50 +148,60 @@ def diameter_filter(diameter_image, length_image, objects_image,
     else:
         perc_filter = max_perc_filter
     
-    ##### Max, min diameter filter ####
+    #### Max, min diameter filter ####
     if pixel_level is True:
-        max_diam_filter = diameter_image < max_diameter
-        min_diam_filter = diameter_image > min_diameter
+        max_diam_filter = diameter < max_diameter
+        min_diam_filter = diameter > min_diameter
         diam_filter = max_diam_filter * min_diam_filter * perc_filter
     else:
         diam_filter = perc_filter
     
     #### Update the skeletons ####
-    new_diam_skeleton = diameter_image * diam_filter
-    new_len_skeleton = length_image * diam_filter
+    new_diam_skeleton = diameter * diam_filter
+    new_len_skeleton = length * diam_filter
     new_labels = labels * diam_filter
     
-    # Re-calculate geometry
-    width_list = ndimage.mean(new_diam_skeleton, 
-                              new_labels, 
-                              index=range(new_labels.max()+1)) 
-    len_list = ndimage.sum(new_len_skeleton,
-                           new_labels,
-                           index=range(new_labels.max()+1))
+    #### Update geometry dataframe ####
+    # Re-calculate geometry of each object
+    objects_diameter = ndimage.mean(new_diam_skeleton, 
+                                    new_labels, 
+                                    index=range(new_labels.max()+1)) 
+    objects_length = ndimage.sum(new_len_skeleton,
+                                 new_labels,
+                                 index=range(new_labels.max()+1))
     
     # Create a new geometry dataframe
-    geom_out = pd.DataFrame({'Length' : len_list,
-                             'Diameter' : width_list
+    geom_out = pd.DataFrame({'Length' : objects_length,
+                             'Diameter' : objects_diameter
                              })
-    geom_out = geom_out[geom_out['Diameter'].notnull()]  # subset only kept objects
+    geom_out = geom_out[geom_out['Diameter'].notnull()]  # subset only present objects
     
-    #### update the objects ###
-    labels, labels_ls = ndimage.label(objects_image) # make labels or original objects
+    #### update the objects ####
+    labels, labels_ls = ndimage.label(objects_in) # make labels or original objects
     new_objects = np.in1d(labels, np.unique(new_labels)) # only keep labels that were kept
-    new_objects = np.reshape(new_objects, new_labels.shape) * labels
+    new_objects = np.reshape(new_objects, new_labels.shape) * labels > 0 # maintain as binary
     
-    return(new_objects, new_diam_skeleton, new_len_skeleton, geom_out)
+    out = {"objects"  : new_objects,
+           "length"   : new_len_skeleton,
+           "diameter" : new_diam_skeleton,
+           "geometry" : geom_out}
     
-def length_width_filter(binary_image, geometry, threshold=5):
+    return(out)
+    
+def length_width_filter(skeleton_dictionary, threshold=5):
     """
     Remove objects based on length:(average) width ratios from skeletonized images.
     
     Parameters
     ----------
-    binary_image : array
-        In input binary image
-    geometry : array
-        n*2 array of geometry for each object, or pandas ``DataFrame``
+    skeleton_dictionary : dict
+        Standard dictionary of objects returned from ``pyroots.skeleton_with_distance``.
+        Items are: 
+            * "objects", a binary image of objects
+            * "length", an ndarray of medial axis, with each pixel representing length
+            * "diameter", an ndarray of medial axis, with each pixel representing diameter
+            * "geometry", a pandas ``DataFrame`` of total length and average diameter for
+            each object.
     threshold : float
         Minimum length:width ratio to keep an object. Default = 5.
     
@@ -199,33 +211,41 @@ def length_width_filter(binary_image, geometry, threshold=5):
     have a large enough length:width threshold. 2) is the updated geometry dataframe.
     
     """    
-
-    geometry = [geometry['Diameter'].values, geometry['Length'].values]
-    labels, labels_ls = ndimage.label(binary_image)
     
-    if labels_ls + 1 is not len(geometry[0]):
+    diameter_in = skeleton_dictionary["diameter"]
+    length_in = skeleton_dictionary["length"]
+    objects_in = skeleton_dictionary["objects"]
+    geometry_in = skeleton_dictionary["geometry"]
+    
+    # convert pandas.DataFrame to list
+    geometry = [geometry_in['Diameter'].values, geometry_in['Length'].values]
+    
+    
+    labels, labels_ls = ndimage.label(skeleton_dictionary["objects"])
+    
+    if labels_ls + 1 is not len(geometry_in.index):
         return("Incompatible Geometry Array and Image")
     
-    #Calculate length:width ratios in the geom array and test whether they pass
-    ratio = geometry[1] / (geometry[0]+0.00001)
+    # Calculate length:width ratios in the geom array and test whether they pass
+    ratio = geometry_in['Length'] / (geometry_in['Diameter']+0.000001)
+    
     thresh_test = ratio > threshold
     
-    #convert the labels to a boolean taking the value from thresh_test based on
-    #the index defined by labels
-    out = thresh_test[labels]
+    # Update geometry dataframe
+    geom_out = geometry_in[thresh_test]
+    geom_out.loc[0] = np.array([0,0])  # re-insert empty space index.
+    geom_out = geom_out.sort_index()
     
-    #scalar multiply to remove geometry of objects that don't pass the threshold
-    geom_thresh = geometry * thresh_test
-    its = range(len(geom_thresh))
+    # Update objects dataframe. Convert the labels to a boolean by determining whether
+    # the object number is true for thresh_test
+    new_objects = np.array(thresh_test)[labels]  
     
-    geom_out = [i for i in its] #placeholder
-    for i in its:
-        geom_out[i] = np.insert(
-            [j for j in geom_thresh[i] if j != 0], #remove rows equal to zero
-            0,0) #re-add first row of zeros for compatibilty with ndimage.label
+    new_diam_skeleton = diameter_in * new_objects
+    new_length_skeleton = length_in * new_objects
     
-    geom_out = pd.DataFrame({'Length' : geom_out[1],
-                             'Diameter' : geom_out[0]
-                             })
+    out = {"objects"  : new_objects,
+           "length"   : new_length_skeleton,
+           "diameter" : new_diam_skeleton,
+           "geometry" : geom_out}
     
-    return(out, geom_out)
+    return(out)
--- pyroots/skeletonization.py
+++ pyroots/skeletonization.py
@@ -18,191 +18,192 @@ from skimage import morphology
 
 
 def _axis_length(image, labels=None, random=True, m=0.5):
-	"""
-	Calculate the length of each pixel in a non-straight line, based on
-	results in Kimura et al. (1999) [1]. This is a modified version of 
-	skimage.morphology.perimeter [2].
-	
-	Parameters
-	----------
-	image : array
-		skeletonized array. Binary image (required)
-	labels : array 
-		Array of object labels. Integer values, for example from 
-		``ndimage.label``. Default=None, which returns a single length 
-		value for all objects in the image. If labels are given, then the
-		total length of each object is returned.
-	random : bool
-		Are the segments randomly oriented, or are they well ordered? 
-		Default=``True``. 
-	m : float
-		Withn range [0,1]. Change if random=``False``, otherwise the algorithm
-		will underestimate length (slightly). Default=0.5 This should work for 
-		most situations. See [1]. 
-		
-		
-	Returns
-	-------
-	length : float
-		If no labels are included, the function returns the total length of
-		all objects in binary image.
-		or
-	length : ndarray
-		If a label array is included, the function returns an array of lengths
-		for each labelled segment.
-		
-	See Also
-	--------
-	skimage.morphology.filter
-	
-	References
-	----------
-	.. [1] Kimura K, Kikuchi S, Yamasaki S. 1999. Accurate root length 
-		   measurement by image analysis. Plant and Soil 216: 117–127.
-	.. [2] K. Benkrid, D. Crookes. Design and FPGA Implementation of
-		   a Perimeter Estimator. The Queen's University of Belfast.
-		   http://www.cs.qub.ac.uk/~d.crookes/webpubs/papers/perimeter.doc
-	
-	"""
-		
-	image = image.astype(np.uint8)
+    """
+    Calculate the length of each pixel in a non-straight line, based on
+    results in Kimura et al. (1999) [1]. This is a modified version of 
+    skimage.morphology.perimeter [2].
+    
+    Parameters
+    ----------
+    image : array
+        skeletonized array. Binary image (required)
+    labels : array 
+        Array of object labels. Integer values, for example from 
+        ``ndimage.label``. Default=None, which returns a single length 
+        value for all objects in the image. If labels are given, then the
+        total length of each object is returned.
+    random : bool
+        Are the segments randomly oriented, or are they well ordered? 
+        Default=``True``. 
+    m : float
+        Withn range [0,1]. Change if random=``False``, otherwise the algorithm
+        will underestimate length (slightly). Default=0.5 This should work for 
+        most situations. See [1]. 
+        
+        
+    Returns
+    -------
+    length : float
+        If no labels are included, the function returns the total length of
+        all objects in binary image.
+        or
+    length : ndarray
+        If a label array is included, the function returns an array of lengths
+        for each labelled segment.
+        
+    See Also
+    --------
+    skimage.morphology.filter
+    
+    References
+    ----------
+    .. [1] Kimura K, Kikuchi S, Yamasaki S. 1999. Accurate root length 
+           measurement by image analysis. Plant and Soil 216: 117–127.
+    .. [2] K. Benkrid, D. Crookes. Design and FPGA Implementation of
+           a Perimeter Estimator. The Queen's University of Belfast.
+           http://www.cs.qub.ac.uk/~d.crookes/webpubs/papers/perimeter.doc
+    
+    """
+        
+    image = image.astype(np.uint8)
 
-	#Define the connectivity list
-	if random is True:
-		pixel_weight = np.array( 
-			#Generated from below, to weight the sum values of connectivity 
-			#from the kernel passed to ndi.convolve
-			[0.   ,  0.5  ,  0.   ,  0.474,  0.   ,  0.948,  0.   ,  1.422,
-			 0.   ,  1.896,  0.   ,  0.67 ,  0.   ,  1.144,  0.   ,  1.618,
-			 0.   ,  2.092,  0.   ,  2.566,  0.   ,  1.341,  0.   ,  1.815,
-			 0.   ,  2.289,  0.   ,  2.763,  0.   ,  3.237,  0.   ,  2.011,
-			 0.   ,  2.485,  0.   ,  2.959,  0.   ,  3.433,  0.   ,  3.907,
-			 0.   ,  2.681,  0.   ,  3.155,  0.   ,  3.629,  0.   ,  4.103,
-			 0.   ,  4.577])
+    #Define the connectivity list
+    if random is True:
+        pixel_weight = np.array( 
+            #Generated from below, to weight the sum values of connectivity 
+            #from the kernel passed to ndi.convolve
+            [0.   ,  0.5  ,  0.   ,  0.474,  0.   ,  0.948,  0.   ,  1.422,
+             0.   ,  1.896,  0.   ,  0.67 ,  0.   ,  1.144,  0.   ,  1.618,
+             0.   ,  2.092,  0.   ,  2.566,  0.   ,  1.341,  0.   ,  1.815,
+             0.   ,  2.289,  0.   ,  2.763,  0.   ,  3.237,  0.   ,  2.011,
+             0.   ,  2.485,  0.   ,  2.959,  0.   ,  3.433,  0.   ,  3.907,
+             0.   ,  2.681,  0.   ,  3.155,  0.   ,  3.629,  0.   ,  4.103,
+             0.   ,  4.577])
 
-#	   weight_seq = np.arange(0, 50, step=1, 
-#							  dtype = "float")	 
-#	   horiz_vert = (weight_seq % 10) // 2	#Count horiz, vertical connects
-#	   diag = weight_seq // 10				#Count diagonal connections
-#	   pixel_weight = 0.5*(horiz_vert + diag*(2**0.5))
-#	   pixel_weight = pixel_weight*0.948	  #Correction factor assuming
-#												 #random orientations of lines 
-#	   pixel_weight[np.arange(0, 50, 2)] = 0  #Only odd numbers are on skeleton
-#	   pixel_weight[1] = 0.5				  #Account for lone pixels
-#			  
-												  
-	else:	#This weighting systematically overestimates (slightly), but the 
-			 #error should be smaller for well-oriented roots. See Kimura et al.
-		 	 #(1999)
-		if m is 0.5:
-			pixel_weight = np.array(
-			#Generated from the else: statement below for m = 0.5
-			[0.   ,  0.5  ,  0.   ,  0.5  ,  0.   ,  1.   ,  0.   ,  1.5  ,
-			 0.   ,  2.   ,  0.   ,  0.707,  0.   ,  1.151,  0.   ,  1.618,
-			 0.   ,  2.096,  0.   ,  2.581,  0.   ,  1.414,  0.   ,  1.851,
-			 0.   ,  2.303,  0.   ,  2.766,  0.   ,  3.236,  0.   ,  2.121,
-			 0.   ,  2.555,  0.   ,  3.   ,  0.   ,  3.454,  0.   ,  3.915,
-			 0.   ,  2.828,  0.   ,  3.26 ,  0.   ,  3.702,  0.   ,  4.15 ,
-			 0.   ,  4.606])
-		else:
-			weight_seq = np.arange(0, 50, step=1, dtype="float")
-			orth = (weight_seq % 10) // 2  #count orthogonal links
-			diag = weight_seq // 10		#count diagonal links
-			pixel_weight = 0.5 * ((diag**2 + (diag + orth*m)**2)**0.5 + orth*(1-m)) 
-			pixel_weight[np.arange(0, 50, 2)] = 0  #Only odd numbers are on skeleton
-			pixel_weight[1] = 0.5				  #Account for lone pixels
+#       weight_seq = np.arange(0, 50, step=1, 
+#                              dtype = "float")     
+#       horiz_vert = (weight_seq % 10) // 2    #Count horiz, vertical connects
+#       diag = weight_seq // 10                #Count diagonal connections
+#       pixel_weight = 0.5*(horiz_vert + diag*(2**0.5))
+#       pixel_weight = pixel_weight*0.948      #Correction factor assuming
+#                                                 #random orientations of lines 
+#       pixel_weight[np.arange(0, 50, 2)] = 0  #Only odd numbers are on skeleton
+#       pixel_weight[1] = 0.5                  #Account for lone pixels
+#              
+                                                  
+    else:    #This weighting systematically overestimates (slightly), but the 
+             #error should be smaller for well-oriented roots. See Kimura et al.
+              #(1999)
+        if m is 0.5:
+            pixel_weight = np.array(
+            #Generated from the else: statement below for m = 0.5
+            [0.   ,  0.5  ,  0.   ,  0.5  ,  0.   ,  1.   ,  0.   ,  1.5  ,
+             0.   ,  2.   ,  0.   ,  0.707,  0.   ,  1.151,  0.   ,  1.618,
+             0.   ,  2.096,  0.   ,  2.581,  0.   ,  1.414,  0.   ,  1.851,
+             0.   ,  2.303,  0.   ,  2.766,  0.   ,  3.236,  0.   ,  2.121,
+             0.   ,  2.555,  0.   ,  3.   ,  0.   ,  3.454,  0.   ,  3.915,
+             0.   ,  2.828,  0.   ,  3.26 ,  0.   ,  3.702,  0.   ,  4.15 ,
+             0.   ,  4.606])
+        else:
+            weight_seq = np.arange(0, 50, step=1, dtype="float")
+            orth = (weight_seq % 10) // 2  #count orthogonal links
+            diag = weight_seq // 10        #count diagonal links
+            pixel_weight = 0.5 * ((diag**2 + (diag + orth*m)**2)**0.5 + orth*(1-m)) 
+            pixel_weight[np.arange(0, 50, 2)] = 0  #Only odd numbers are on skeleton
+            pixel_weight[1] = 0.5                  #Account for lone pixels
 
-	#Run the connectivity kernel	
-	kernel = np.array([[10, 2, 10],
-					   [ 2, 1,  2],
-					   [10, 2, 10]])
+    #Run the connectivity kernel    
+    kernel = np.array([[10, 2, 10],
+                       [ 2, 1,  2],
+                       [10, 2, 10]])
 
-	kernel_out = ndimage.convolve(image, kernel,
-								  mode='constant', cval=0
-								  )
-	
-	#convert kernel_out to pixel length, for diagnostics
-	dims = kernel_out.shape
-	pixel_length = np.ones(dims)	#most should become zeros
-	for i in range(dims[0]):		#x dimension
-		for j in range(dims[1]):	#y dimension
-			pixel_length[i,j] = pixel_weight[kernel_out[i,j]] 
-				#match the value to that in the pixel_weight list
-			
-	if labels is None: 
-		
-		# From original code - skimage.morphology.perimeter():
-		# "You can also write
-		# return perimeter_weights[perimeter_image].sum()
-		# but that was measured as taking much longer than bincount + np.dot 
-		# (5x as much time)"
-		weight_bins = np.bincount(kernel_out.ravel(), minlength=50)
-		length_out = np.dot(weight_bins, pixel_weight)
-		return(weight_bins, 
-			   length_out)
-	else:
-		dims = kernel_out.shape
-		pixel_length = np.zeros(dims)	#most should stay zeros
-		for i in range(dims[0]):		#x dimension iterate
-			for j in range(dims[1]):	#y dimension iterate
-				pixel_length[i,j] = pixel_weight[kernel_out[i,j]] 
-		length_out = ndimage.sum(pixel_length, 
-								 labels=labels, 
-								 index=range(labels.max() + 1)[1:]
-								 #might want to ignore index 0 
-								 #for large images.
-								 )
-		return(pixel_length,
-			   length_out)
-			   
-			   
+    kernel_out = ndimage.convolve(image, kernel,
+                                  mode='constant', cval=0
+                                  )
+    
+    #convert kernel_out to pixel length, for diagnostics
+    dims = kernel_out.shape
+    pixel_length = np.ones(dims)    #most should become zeros
+    for i in range(dims[0]):        #x dimension
+        for j in range(dims[1]):    #y dimension
+            pixel_length[i,j] = pixel_weight[kernel_out[i,j]] 
+                #match the value to that in the pixel_weight list
+            
+    if labels is None: 
+        
+        # From original code - skimage.morphology.perimeter():
+        # "You can also write
+        # return perimeter_weights[perimeter_image].sum()
+        # but that was measured as taking much longer than bincount + np.dot 
+        # (5x as much time)"
+        weight_bins = np.bincount(kernel_out.ravel(), minlength=50)
+        length_out = np.dot(weight_bins, pixel_weight)
+        return(weight_bins, 
+               length_out)
+    else:
+        dims = kernel_out.shape
+        pixel_length = np.zeros(dims)    #most should stay zeros
+        for i in range(dims[0]):        #x dimension iterate
+            for j in range(dims[1]):    #y dimension iterate
+                pixel_length[i,j] = pixel_weight[kernel_out[i,j]] 
+        length_out = ndimage.sum(pixel_length, 
+                                 labels=labels, 
+                                 index=range(labels.max() + 1)[1:]
+                                 #might want to ignore index 0 
+                                 #for large images.
+                                 )
+        return(pixel_length,
+               length_out)
+               
+               
 def skeleton_with_distance(img, random=True, m=0.5):
-	"""
-	Created on Thu 21 Jul 2016 03:27:52 PM CDT 
-	
-	@author: pme
+    """
+    Created on Thu 21 Jul 2016 03:27:52 PM CDT 
+    
+    @author: pme
 
-	Calls ``morphology.medial_axis``, then calculates the length of each axis.
+    Calls ``morphology.medial_axis``, then calculates the length of each axis
+    using ``pyroots._axis_length``
 
-	Parameters
-	--------
-	img: array
-		boolean ndarray of objects
-	random: boolean
-		Are the segments randomly oriented? Default=``True``. 
-		Feeds ``pyroots.axis_length``. 
-	m: float
-		range [0,1]. Feeds ``pyroots.axis_length``. Default = 0.5. Change only
-		if random=``False``.
+    Parameters
+    --------
+    img: array
+        boolean ndarray of objects
+    random: boolean
+        Are the segments randomly oriented? Default=``True``. 
+        Feeds ``pyroots.axis_length``. 
+    m: float
+        range [0,1]. Feeds ``pyroots._axis_length``. Default = 0.5. Change only
+        if ``random=False``.
 
-	Returns
-	--------
-	A list containing:
-	1. An ndarray of length for each pixel
-	2. An ndarray of diameter at each pixel
-	3. A list of vectors (mean length, diameter) for each object. The 
-		first index is open space and therefore 0 (for consistency with 
-		ndimage.label() behavior). 
-		
-	"""
-	
-	labels, labels_ls = ndimage.label(img)
-	skel, dist = morphology.medial_axis(img, return_distance=True)
-	dist_img = skel*2*dist #2x because medial axis distance is radius
-	label_skel = skel*labels
-	
-	width_list = ndimage.mean(dist_img, 
-							  label_skel, 
-							  index=range(labels_ls+1)[1:]) #ignore empty space
-	
-	length_img, length_list = _axis_length(skel, labels, random=random, m=m)
-	geom_df = pd.DataFrame({"Length" : np.insert(length_list, 0, 0), #to re-add the label index, 0
-				  "Diameter" : np.insert(width_list, 0, 0)
-				}) 
-	
+    Returns
+    --------
+    A dictionary containing:
+    1. "objects" : The objects image, ``img``
+    2. "length" : An ndarray of length for each pixel
+    3. "diameter" : An ndarray of diameter at each pixel
+    4. "geometry" : A pandas dataframe listing the mean length and mean diameter
+    for each object (including the open space)
+        
+    """
+    
+    labels, labels_ls = ndimage.label(img)
+    skel, dist = morphology.medial_axis(img, return_distance=True)
+    dist_img = skel*2*dist #2x because medial axis distance is radius
+    label_skel = skel*labels
+    
+    width_list = ndimage.mean(dist_img, 
+                              label_skel, 
+                              index=range(labels_ls+1)[1:]) #ignore empty space
+    
+    length_img, length_list = _axis_length(skel, labels, random=random, m=m)
+    geom_df = pd.DataFrame({"Length" : np.insert(length_list, 0, 0), #to re-add the label index, 0
+                            "Diameter" : np.insert(width_list, 0, 0)
+                            }) 
+    
+    out = {"objects"  : img,
+           "length"   : length_img,
+           "diameter" : dist_img,
+           "geometry" : geom_df}
 
-#	lw_ratio = length / (width+0.0000001)
-#	lw_filter_img = 
-	
-	
-	return(length_img, dist_img, geom_df)
+    return(out)
