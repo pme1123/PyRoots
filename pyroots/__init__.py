8134880ac86e20bd27dc6f74265f294424103ac5
pme1123 <pmewing87@gmail.com>
2017-01-17 16:02:15 -0600

    equalize_exposure

--- pyroots/__init__.py
+++ pyroots/__init__.py
@@ -2,7 +2,7 @@ from .noise_filters import noise_removal, dirt_removal, grayscale_filter, color_
 from .geometry_filters import _percentile_filter, diameter_filter, length_width_filter, morphology_filter, hollow_filter
 from .summarize import summarize_geometry, bin_by_diameter
 from .skeletonization import _axis_length, skeleton_with_distance
-from .image_manipulation import img_split, ellipse_mask
+from .image_manipulation import img_split, ellipse_mask, equalize_exposure
 from .utilities import multi_image_plot, random_blobs, tiff_splitter, band_viewer, _zoom
 from .example_functions import pyroots_analysis, image_loop
 
@@ -10,6 +10,6 @@ __all__ = ['noise_removal', 'dirt_removal', 'grayscale_filter', 'color_filter', 
 		   '_percentile_filter', 'diameter_filter', 'length_width_filter', 'morphology_filter', 'hollow_filter',
 	   	   'summarize_geometry', 'bin_by_diameter', 
 	   	   '_axis_length', 'skeleton_with_distance',
-	   	   'img_split', 'ellipse_mask',
+	   	   'img_split', 'ellipse_mask', 'equalize_exposure',
 	   	   'multi_image_plot', 'random_blobs', 'tiff_splitter', 'band_viewer', '_zoom',
 	   	   'pyroots_analysis', 'image_loop']
--- pyroots/image_manipulation.py
+++ pyroots/image_manipulation.py
@@ -11,7 +11,105 @@ Contents:
 """
 
 import numpy as np
-from skimage import draw
+from skimage import draw, morphology, filters, exposure, img_as_float
+
+
+#########################################################################################################################
+#########################################################################################################################
+#######                                                                                                          ########
+#######                                            Equalize Exposure                                             ########
+#######                                                                                                          ########
+#########################################################################################################################
+#########################################################################################################################
+
+
+def equalize_exposure(image, kernel_size=None, min_object_size=500, dark_objects=True, stretch=False):
+    """
+    Filter a grayscale image with uneven brightness across it, such as you might see in a microscope image.
+    Removes large objects using adaptive thresholding based on `min_object_size`, then calculates the mean
+    in a circular neighborhood of diameter `kernel_size`. Smooths this mean, then subtracts the background
+    variation from the original image (including large objects). Run twice of best results, though once 
+    should give satisfactory results. As a bonus, this often enhances white balance and colors.
+    
+    For color images, run on each band separately and then combine into a [dim_x, dim_y, 3] numpy array.
+    When run on color images, this function improves white balance and colors.
+    
+    Essential for filtering candidate objects by color. Slow; could be optimized with `opencv_python`, though
+    this function doesn't support masking when calculating means.
+    
+    Parameters
+    ----------
+    image : ndarray (float, int)
+        Grayscale image or band.
+    kernel_size : int
+        Passes to `skimage.morphology.disk` to create a kernel of diameter kernel_size in pixels. If `None`,
+        defaults to `max(image.shape)/10`.
+    min_object_size : int
+        Passes to `skimage.morphology.remove_small_holes`. Area of objects to ignore when averaging
+        background values, in pixels.
+    dark_objects : bool
+        Are objects dark against a light background?
+    stretch : bool
+        Stretch values to cover entire colorspace? Enhances colors. Largely aesthetic. Not recommended
+        for batch analyses.
+        
+    Returns
+    -------
+    An ndarray of type float [0:1].
+    
+    See Also
+    --------
+    `skimage.filters.rank.mean`
+    """
+    
+    # Housekeeping
+    img = img_as_float(image.copy())
+    
+    if stretch is True:
+        img = img/img.max()
+    
+    if dark_objects is False:
+        img = 1-img  # invert
+        
+    img_in = img.copy()  # for use later
+    
+    if kernel_size is None:
+        kernel_size = np.int(max(image.shape[0], image.shape[1])/10)
+    
+    # mean filter kernel
+    kernel = morphology.disk(int(kernel_size/2))
+    
+    # identify objects to ignore
+    if kernel_size % 2 is 0:
+        block_size = kernel_size + 1
+    else:
+        block_size = kernel_size
+    
+    objects = ~filters.threshold_adaptive(img, block_size, offset = 0.01*img.max())
+    objects = morphology.remove_small_objects(objects, min_size = min_object_size)
+    
+    # Global mean
+    img_mean = np.ma.masked_array(img, mask = objects).mean()
+
+    # Local means
+    local_means = filters.rank.mean(img, selem=kernel, mask=~objects)
+    local_means = filters.gaussian(local_means, kernel_size)
+    
+    #correction
+    out = img_in + (img_mean - local_means)
+    out = img_as_float(out)
+    
+    return(out)
+
+
+
+#########################################################################################################################
+#########################################################################################################################
+#######                                                                                                          ########
+#######                                             Image Splitter                                               ########
+#######                                                                                                          ########
+#########################################################################################################################
+#########################################################################################################################
 
 def img_split(img):
 
@@ -41,6 +139,14 @@ def img_split(img):
 		return(band1, band2, band3, band4)
 	return(band1, band2, band3)
 
+
+#########################################################################################################################
+#########################################################################################################################
+#######                                                                                                          ########
+#######                                             Elliptical Mask                                              ########
+#######                                                                                                          ########
+#########################################################################################################################
+#########################################################################################################################
 
 def ellipse_mask(img, percentage_x=100, percentage_y=100, offset_x=0, offset_y=0, rotation=0):
     """
