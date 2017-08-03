e039eccd546d8a6c44ef6a401d11435ed780306d
pme1123 <pmewing87@gmail.com>
2017-08-03 18:46:34 -0400

    update to allow black and white images

--- pyroots/thresholding_segmentation.py
+++ pyroots/thresholding_segmentation.py
@@ -10,9 +10,6 @@ Contents:
 TODO: Test multi_image_loop
 """
 
-import os
-from PIL import Image
-import pandas as pd
 from pyroots import *
 from skimage import io, color, filters, exposure, img_as_ubyte
 from warnings import warn
@@ -20,7 +17,7 @@ from warnings import warn
 def thresholding_segmentation(image,
                               threshold_args,
                               image_name='Default Image',
-                              colors='skip',
+                              colors='dark',
                               contrast_kernel_size='skip',
                               mask_args='skip',
                               noise_removal_args='skip',
@@ -30,7 +27,6 @@ def thresholding_segmentation(image,
                               diam_filter_args='skip',
                               diameter_bins=None,
                               verbose=False):
-    #TODO: Test
     """
     Full analysis of an image for length of objects based on thresholding.
     Performs the following steps:
@@ -60,19 +56,23 @@ def thresholding_segmentation(image,
     image_name : str
     	What do you want to call your image?
 
-    colors : dict
-        Currently only supports one colorspace, but you can choose multiple bands.
-    	A dictionary containing:
-        - colorspace: string.
-            Colorspace in which to run the analysis (ex. RGB, LAB, HSV). See
-            scikit-image documentation for all options.
-        - band: list of integers
-            Specifying which bands of `colorspace` on which to run the analysis
-            (ex. 2 in RGB gives Blue, 0 gives Red).
-        - dark_on_light: list of boolean
-            Are the objects dark objects on a light background? Length must match
-            length of `colors['band']`.
-        `skip` for a black and white image.
+    colors : dict or string
+        For color analysis:
+            Currently only supports one colorspace, but you can choose multiple bands.
+        	A dictionary containing:
+            - colorspace: string.
+                Colorspace in which to run the analysis (ex. RGB, LAB, HSV). See
+                scikit-image documentation for all options.
+            - band: list of integers
+                Specifying which bands of `colorspace` on which to run the analysis
+                (ex. 2 in RGB gives Blue, 0 gives Red).
+            - dark_on_light: list of boolean
+                Are the objects dark objects on a light background? Length must match
+                length of `colors['band']`.
+        For black and white analysis:
+            A string of either:
+                `'dark'` for dark roots on a light background
+                `'light'` for light roots on a dark background
 
     contrast_kernel_size : int or None
         Dimension of kernel for adaptively enhancing contrast. Calls
@@ -119,30 +119,60 @@ def thresholding_segmentation(image,
             - the diameter at that pixel (if `diameter_bins` is `None`)
             - the diameter bin to which a pixel belongs (otherwise)
     3) skeleton pixel lengths; 4) skeleton pixel diameters.
-
+    
+    Notes
+    -----
+    Most functions within this method are attempted. If they receive an unuseable argument, 
+    e.g. the dictionary contains a formatting error or a bad keyword, then the method will be 
+    skipped with a warning. If you see such a warning ("Skipping (function)..."), check the 
+    formatting of your argument and re-create a parameters file with a jupyter notebook.
+    
     See Also
     --------
     For example parameter dictionaries, see example_thresholding_analysis_parameters.py.
 
     """
 
-    # Housekeeping
-    ## count number of bands and make sure the items 'band' and 'dark_on_light' are lists
-    try:
-        nbands = len(colors['band'])
-    except:
-        colors['band'] = [colors['band']]
-        nbands = len(colors['band'])
-        pass
 
-    try:
-        len(colors['dark_on_light'])
-    except:
-        colors['dark_on_light'] = [colors['dark_on_light']]
-        if nbands != len(colors['dark_on_light']):
-            raise ValueError("Number of items in `colors['dark_on_light']` doesn't\
-                             equal the number of bands in `colors['band']`!")
-        pass
+    # Begin
+    ## Convert Colorspace, enhance contrast
+    if len(colors) == 3:  #then it's an RGB image
+        #housekeeping
+        nbands = len(colors['band'])
+        try:
+            len(colors['dark_on_light'])
+        except:
+            colors['dark_on_light'] = [colors['dark_on_light']]
+            if nbands != len(colors['dark_on_light']):
+                raise ValueError("Number of items in `colors['dark_on_light']` doesn't\
+                                 equal the number of bands in `colors['band']`!")
+            pass
+
+        try:
+            len(colors['band'])
+        except: 
+            colors['band'] = [colors['band']]
+            pass
+
+        # convert colorspace if necessary
+        try:
+            working_image = getattr(color, "rgb2" + colors['colorspace'].lower())(image)
+        except:
+            working_image = image.copy()
+            if colors['colorspace'].lower() != 'rgb':
+                raise ValueError("Didn't recognize specified colorspace. Skipping conversion")
+            pass
+        
+        # pull bands
+        working_image = [pr.img_split(working_image)[i] for i in colors['band']]
+        
+    
+    else:  # it's a black and white image
+        nbands = 1
+        working_image = [image.copy()]
+        if len(image.shape) != 2:
+            raise ValueError("Your `color` argument suggested a grayscale image, but it has \
+            multiple bands!")
 
     ## Count nubmer of dictionaries in threshold_args. Should equal number of bands. Make sure is list.
     try:
@@ -152,21 +182,8 @@ def thresholding_segmentation(image,
         if nbands != len(threshold_args):
             raise ValueError("Number of dictionaries in `threshold_args` doesn't\
                              equal the number of bands in `colors['band']`!")
-        pass
+        pass        
 
-    # Begin
-    ## Convert Colorspace, enhance contrast
-    try:  # detects if color=None
-        if colors['colorspace'].lower() != 'rgb':
-            working_image = getattr(color, "rgb2" + colors['colorspace'])(image)
-        else:
-            working_image = image.copy()
-        working_image = [img_split(working_image)[i] for i in colors['band']] # pull bands
-    except:  # for black and white
-        if colors is not 'skip':
-            raise ValueError("Your colors arguments are invalid. For black and white, set colors=None.")
-        working_image = image.copy()
-        pass
     if verbose is True:
         print("Analysis bands selected")
 
@@ -186,11 +203,14 @@ def thresholding_segmentation(image,
     for i in range(nbands):
         working_image[i] = working_image[i] > filters.threshold_local(working_image[i],
                                                                       **threshold_args[i])
-
     for i in range(nbands):
-        if colors['dark_on_light'][i] is True:
-            working_image[i] = ~working_image[i]
-
+        if len(colors) == 3:
+            if colors['dark_on_light'][i] is True:
+                working_image[i] = ~working_image[i]
+        else:
+            if colors == 'dark':
+                working_image[i] = ~working_image[i]
+                
     ## Combine bands. As written, keeps all 'TRUE'
     combined = working_image[0].copy()
     for i in range(1, nbands):
@@ -281,58 +301,3 @@ def thresholding_segmentation(image,
         print("Done")
 
     return(out)
-
-def thresholding_image_loop(root_directory, image_extension, path_to_params, mask=None, save_images=False):
-    """
-    Reference function to loop through images in a directory. As it is written, it returns
-    a dataframe from "pyroots_analysis" and also writes images showing the objects analyzed.
-    """
-    exec(open(path_to_params).read())
-    #Make an output directory for the analyzed images and data. Requires os.
-    if save_images is True:
-        if not os.path.exists(root_directory + os.sep + "Pyroots Analyzed"):
-            os.mkdir(root_directory + os.sep + "Pyroots Analyzed")
-
-    #Make a placeholder DataFrame for the output data. Requires pandas.
-    if diameter_bins is None:
-        img_df = pd.DataFrame(columns=("ImageName", "Length", "NObjects", "MeanDiam"))
-    else:
-        img_df = pd.DataFrame(columns=("ImageName", "DiameterClass", "Length"))
-
-    #Begin looping
-    for subdir, dirs, files in os.walk(root_directory):
-        for file_in in files:
-
-            #criteria for doing something
-            if file_in.endswith(image_extension) and not subdir.endswith("Pyroots Analyzed"):
-                filepath = subdir + os.sep + file_in  # what's the image called and where is it?
-
-                #Create filein name
-                filein = "DIR" + subdir[len(root_directory):] + os.sep + file_in
-                #Import image
-                img = io.imread(filepath)[:,:,0:3]  # load image
-
-                #Run through the analysis. Requires pyroots.pyroots_analysis reference function.
-                temp_df, objects, length_skel, dist_skel = pr.pyroots_analysis(
-                    image = img,
-                    image_name = file_in,
-                    colorspace = colorspace,
-                    analysis_bands = analysis_bands,
-                    threshold_params = threshold_params,
-                    mask = mask,
-                    filtering_params = filtering_params,
-                    light_on_dark = light_on_dark,
-                    diameter_bins = diameter_bins,
-                    optimize = False
-                )
-
-                #Paste the data into the output dataframe
-                img_df = pd.concat((img_df, temp_df))
-
-                #save images?
-                if save_images is True:
-                    #Convert boolean array to 8-bit, 1-band image. Requires PIL.
-                    im = Image.fromarray((255*~objects).astype('uint8')) #black roots, white background
-                    im.save(root_directory + os.sep + "Pyroots Analyzed" + os.sep + file_in)
-
-    return(img_df)
