adbcdfe6068c1817ad9a4c5365b33fbed51529c8
pme1123 <pmewing87@gmail.com>
2018-05-19 15:32:43 -0700

    add draw_fishnet function

--- pyroots/__init__.py
+++ pyroots/__init__.py
@@ -5,7 +5,7 @@ from .summarize import summarize_geometry, bin_by_diameter
 from .skeletonization import _axis_length, skeleton_with_distance
 from .image_manipulation import img_split, draw_mask, equalize_exposure, _arrays_mean, _arrays_var, calc_exposure_correction, _center_image, fill_gaps, band_selector
 from .preprocessing import detect_motion_blur, calc_temperature_distance, correct_brightfield, register_bands, preprocessing_filters, preprocessing_actions
-from .utilities import multi_image_plot, random_blobs, tiff_splitter, band_viewer, _zoom, img_rescaler
+from .utilities import multi_image_plot, random_blobs, tiff_splitter, band_viewer, _zoom, img_rescaler, draw_fishnet
 from .thresholding_segmentation import thresholding_segmentation
 from .frangi_segmentation import frangi_segmentation
 from .batch_processing import preprocessing_filter_loop, preprocessing_actions_loop, frangi_image_loop, pyroots_batch_loop
@@ -18,7 +18,7 @@ __all__ = ['noise_removal', 'dirt_removal', 'grayscale_filter', 'color_filter', 
 	   	   '_axis_length', 'skeleton_with_distance',
 	   	   'img_split', 'draw_mask', 'equalize_exposure', '_arrays_mean', '_arrays_var', 'calc_exposure_correction', '_center_image', 'fill_gaps', 'band_selector',
 	   	   'detect_motion_blur', 'calc_temperature_distance', 'correct_brightfield', 'register_bands', 'preprocessing_filters', 'preprocessing_actions',
-	   	   'multi_image_plot', 'random_blobs', 'tiff_splitter', 'band_viewer', '_zoom', 'img_rescaler',
+	   	   'multi_image_plot', 'random_blobs', 'tiff_splitter', 'band_viewer', '_zoom', 'img_rescaler', 'draw_fishnet',
 	   	   'thresholding_segmentation',
 	   	   'preprocessing_filter_loop', 'preprocessing_actions_loop', 'frangi_image_loop', 'pyroots_batch_loop',
 	   	   'frangi_segmentation']
